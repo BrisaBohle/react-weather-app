003c9842f5eeedb2d5b49dd7fc20aeb9e552fd65
BrisaBohle <81156231+BrisaBohle@users.noreply.github.com>
2021-06-07 09:47:02 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-
+ 
 export default 
 function App() {
   return (
     <div className="App">
-      Weather App
+<div className="container">
+      <h1>Weather App</h1>
       <footer>
       <a href="https://github.com/BrisaBohle/react-weather-app" target="_blank" rel="nonreferrer">Open-Sounce code</a>, by <a href="https://brisa-bohle.netlify.app/" target="_blank" rel="nonreferrer">Brisa Bøhle</a>
     </footer>
+    </div>
     </div>
   );
 }
