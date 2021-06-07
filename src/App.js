97f86b16cea1867d4f31b1d60cb1967fca8d2c46
BrisaBohle <81156231+BrisaBohle@users.noreply.github.com>
2021-06-07 12:12:34 +0200

    API Call

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
 <div className="container">
-      <Weather />
+      <Weather defaultCity="Oslo"/>
       <footer>
       <a href="https://github.com/BrisaBohle/react-weather-app" target="_blank" rel="noreferrer">Open-Sounce code</a>, by <a href="https://brisa-bohle.netlify.app/" target="_blank" rel="noreferrer">Brisa Bøhle</a>
     </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -1,49 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather(){
-  return (
-  <div className="Weather">
-    <div className="container">
-      <form>
+export default function Weather(props){
+  
+   const [weatherData, setWeatherData] = useState ({ready: false});
+   function handleResponse(response){
+      setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      description: response.data.weather[0].description,
+    });
+      }
+
+  if (weatherData.ready){
+    return (
+    <div className="Weather">
+      <div className="container">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input className="form-control w-100" autoFocus="on"type="search" placeholder="Enter a city..."/>
+            </div>
+            <div className="col-3">
+              <button className="btn btn-primary
+              w-100" value="search" type="submit">Search</button>
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>
+            Weekday 00:00
+          </li>
+          <li className="text-capitalize">
+            {weatherData.description}
+          </li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input className="form-control w-100" autoFocus="on"type="search" placeholder="Enter a city..."/>
+          <div className="col-6 mt-3">
+            <img src={weatherData.iconUrl} alt={weatherData.description}></img>
+            <span className="temperature">{Math.round(weatherData.temperature)}</span>
+            <span className="unit">°C</span>
+            </div>
+          <div className="col-6 mt-3">
+            <ul>
+              <li>
+                Humidity: {weatherData.humidity}%
+              </li>
+              <li>
+                Wind: {weatherData.wind}km/h
+              </li>
+            </ul>
           </div>
-          <div className="col-3">
-            <button className="btn btn-primary w-100" value="search" type="submit">Search</button>
-          </div>
-        </div>
-      </form>
-      <h1>City</h1>
-      <ul>
-        <li>
-          Weekday 00:00
-        </li>
-        <li>
-          Weather condition
-        </li>
-      </ul>
-      <div className="row">
-        <div className="col-6 mt-3">
-          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="img-description"></img>
-          <span className="temperature">6</span>
-          <span className="unit">°C</span>
-          </div>
-        <div className="col-6 mt-3">
-          <ul>
-            <li>
-              Precipitation:50%
-            </li>
-            <li>
-              Humidity: 70%
-            </li>
-            <li>
-              Wind: 7km/h
-            </li>
-          </ul>
         </div>
       </div>
-    </div>
-  </div>);
+    </div>);} 
+  else{ 
+    const apiKey = "5077d3232d041e2158e79942007bee5b";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return"Loading.."
+    }
 }
