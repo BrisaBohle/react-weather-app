bcb4ae48ac349826948b9f6c5a407fdd61a3c1be
BrisaBohle <81156231+BrisaBohle@users.noreply.github.com>
2021-06-07 09:38:13 +0200

    Added footer with link source

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+      <a href="https://github.com/BrisaBohle/react-weather-app" target="_blank" rel="nonreferrer">Open-Sounce code</a>, by <a href="https://brisa-bohle.netlify.app/" target="_blank" rel="nonreferrer">Brisa Bøhle</a>
+    </footer>
     </div>
   );
 }
