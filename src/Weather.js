777e3acc44cf05103d9ec76985ef2aad13842cfb
BrisaBohle <81156231+BrisaBohle@users.noreply.github.com>
2021-06-08 08:46:13 +0200

    http to https

--- src/Weather.js
+++ src/Weather.js
@@ -21,7 +21,7 @@ export default function Weather(props){
       }
       function search(){
 const apiKey = "df68ff14e94919453ad528aab8f6c8b7";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
       }
 function handleSubmit(event){
